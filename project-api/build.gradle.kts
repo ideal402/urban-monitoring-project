da4595f2129f7f8542ef8cdb688ef07c532b80c1
ideal402 <ideal402@naver.com>
2026-01-02 19:57:38 +0900

    add openapi generator

--- project-api/build.gradle.kts
+++ project-api/build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
 
 dependencies {
     implementation(libs.spring.boot.starter.web) 
+    implementation(project(":project-interface"))
 }
--- project-interface/build.gradle.kts
+++ project-interface/build.gradle.kts
@@ -1,7 +1,55 @@
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
 plugins {
     id("java-conventions")
+    alias(libs.plugins.openapi.generator)
 }
 
 dependencies{
-    
+    implementation(libs.spring.boot.starter.web) {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
+    }
+    implementation(libs.swagger.annotations)
+    implementation(libs.jackson.databind.nullable) 
+    implementation(libs.jakarta.validation.api)
+    implementation(libs.jakarta.annotation.api)
+    compileOnly(libs.jakarta.servlet.api)
+}
+
+// OpenAPI Generate 설정
+tasks.named<GenerateTask>("openApiGenerate") {
+    generatorName.set("spring")
+
+    // [경로 설정] 현재 모듈(project-interface) 내부의 파일을 바라봅니다.
+    inputSpec.set(file("src/main/resources/api-spec.yaml").absolutePath)
+
+    // [출력 설정] 현재 모듈의 build 폴더 내부에 생성
+    outputDir.set(layout.buildDirectory.dir("generated/openapi").get().asFile.absolutePath)
+
+    apiPackage.set("com.ideal402.urdan.api.controller")
+    modelPackage.set("com.ideal402.urdan.api.dto")
+
+    configOptions.set(mapOf(
+        "dateLibrary" to "java8",
+        "useSpringBoot3" to "true",
+        "interfaceOnly" to "true",       // 인터페이스만 생성
+        "useTags" to "true",
+        "skipDefaultInterface" to "true",
+        "openApiNullable" to "false",
+        "unhandledException" to "true"
+    ))
+}
+
+// 생성된 소스 코드 인식 설정
+sourceSets {
+    main {
+        java {
+            srcDir(layout.buildDirectory.dir("generated/openapi/src/main/java"))
+        }
+    }
+}
+
+// 컴파일 시점 의존성
+tasks.named("compileJava") {
+    dependsOn("openApiGenerate")
 }
