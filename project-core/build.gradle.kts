ef49b6c6765f3b6b8a686e60c310dbadbfcc06f2
ideal402 <ideal402@naver.com>
2026-01-09 03:27:53 +0900

    feat: MapApi 통합 테스트 추가

--- project-core/build.gradle.kts
+++ project-core/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies{
     implementation(libs.spring.boot.starter.web)
     implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.spring.boot.starter.validation)
     runtimeOnly(libs.h2)
     implementation(project(":project-interface"))
 }
