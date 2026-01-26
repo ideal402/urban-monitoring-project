b6a2aeb5cffec057cc9c0f93d0cc577109f8e314
ideal402 <ideal402@naver.com>
2026-01-27 03:36:07 +0900

    fix: user service 세션 인증 방식에 맞게 변경

--- project-api/src/test/java/com/ideal402/urban/UserApiTest.java
+++ project-api/src/test/java/com/ideal402/urban/UserApiTest.java
@@ -1,157 +1,157 @@
-//package com.ideal402.urban;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.ideal402.urban.api.dto.WithdrawUserRequest;
-//import com.ideal402.urban.common.GlobalExceptionHandler;
-//import com.ideal402.urban.common.ResourceNotFoundException;
-//import com.ideal402.urban.config.SecurityConfig;
-//import com.ideal402.urban.domain.entity.User;
-//import com.ideal402.urban.domain.repository.UserRepository;
-//import com.ideal402.urban.service.UserService;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.http.MediaType;
-//import org.springframework.security.access.AccessDeniedException;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.test.context.bean.override.mockito.MockitoBean;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import static org.mockito.ArgumentMatchers.*;
-//import static org.mockito.BDDMockito.willThrow;
-//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
-//
-//import static org.mockito.BDDMockito.willDoNothing;
-//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@WebMvcTest(UserController.class)
-//@Import({GlobalExceptionHandler.class, SecurityConfig.class})
-//public class UserApiTest {
-//
-//    @Autowired
-//    private ObjectMapper objectMapper;
-//
-//    @MockitoBean
-//    private UserService userService;
-//
-//    @MockitoBean
-//    private UserRepository userRepository;
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//
-//    @MockitoBean
-//    private RedisTemplate<String, String> redisTemplate;
-//
-//
-//    @Test
-//    @DisplayName("setAlarm: 정상 동작")
-//    public void setAlarmTest() throws Exception {
-//        User mockUser = new User("test","test@email","test");
-//
-//        var authToken = new UsernamePasswordAuthenticationToken(mockUser,"test",null);
-//
-//        willDoNothing().given(userService).addAlarm(any(User.class), eq(1));
-//
-//        mockMvc.perform(post("/users/me/alarms/{regionId}", 1)
-//                    .with(authentication(authToken))
-//                    .contentType(MediaType.APPLICATION_JSON)
-//                    .with(csrf()))
-//                .andExpect(status().isCreated())
-//                .andDo(print());
-//    }
-//
-//    @Test
-//    @DisplayName("setAlarm: 존재하지 않는 지역 - 404")
-//    public void setAlarmNotFoundTest() throws Exception {
-//        User mockUser = new User("test","test@email","test");
-//
-//        var authToken = new UsernamePasswordAuthenticationToken(mockUser,"test",null);
-//
-//        willThrow(new ResourceNotFoundException("존재하지 않는 지역입니다."))
-//                .given(userService).addAlarm(any(User.class), eq(1000));
-//
-//        mockMvc.perform(post("/users/me/alarms/{regionId}", 1000)
-//                    .with(authentication(authToken))
-//                    .contentType(MediaType.APPLICATION_JSON)
-//                    .with(csrf()))
-//                .andExpect(status().isNotFound())
-//                .andDo(print());
-//    }
-//
-//    @Test
-//    @DisplayName("deleteAlarm: 정상 요청")
-//    public void deleteAlarmTest() throws Exception {
-//        User mockUser = new User("test","test@email","test");
-//        var authToken = new UsernamePasswordAuthenticationToken(mockUser,"test",null);
-//
-//        willDoNothing().given(userService).deleteAlarm(any(User.class), eq(1));
-//
-//        mockMvc.perform(delete("/users/me/alarms/{regionId}", 1)
-//                .with(authentication(authToken))
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .with(csrf()))
-//                .andExpect(status().isNoContent())
-//                .andDo(print());
-//    }
-//
-//    @Test
-//    @DisplayName("withdrawUser: 정상 요청")
-//    public void withdrawUserTest() throws Exception {
-//        User mockUser = new User("test","test@email","test");
-//        var authToken = new UsernamePasswordAuthenticationToken(mockUser,"test",null);
-//
-//        WithdrawUserRequest withdrawUserRequest = new WithdrawUserRequest().password("test");
-//
-//        willDoNothing().given(userService).withdrawUser(any(User.class), anyString());
-//
-//        mockMvc.perform(delete("/users/me")
-//                    .with(authentication(authToken))
-//                    .contentType(MediaType.APPLICATION_JSON)
-//                    .content(objectMapper.writeValueAsString(withdrawUserRequest))
-//                    .accept(MediaType.APPLICATION_JSON)
-//                    .with(csrf()))
-//                .andExpect(status().isNoContent())
-//                .andDo(print());
-//    }
-//
-//    @Test
-//    @DisplayName("withdrawUser: 비밀번호 불일치 - 403")
-//    public void withdrawUserNoAuthTest() throws Exception {
-//        User mockUser = new User("test","test@email","test");
-//        var authToken = new UsernamePasswordAuthenticationToken(mockUser,"test",null);
-//
-//        WithdrawUserRequest withdrawUserRequest = new WithdrawUserRequest().password("test");
-//
-//        willThrow(new AccessDeniedException("비밀번호가 일치하지 않습니다.")).given(userService).withdrawUser(any(User.class), anyString());
-//
-//        mockMvc.perform(delete("/users/me")
-//                    .with(authentication(authToken)).accept(MediaType.APPLICATION_JSON)
-//                    .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(withdrawUserRequest))
-//                    .with(csrf()))
-//                .andExpect(status().isForbidden())
-//                .andDo(print());
-//    }
-//
-//    @Test
-//    @DisplayName("withdrawUser: 인증토큰 만료 - 401")
-//    public void withdrawUserNoAuthTest2() throws Exception {
-//
-//        WithdrawUserRequest withdrawUserRequest = new WithdrawUserRequest().password("test");
-//
-//        mockMvc.perform(delete("/users/me")
-//                    .contentType(MediaType.APPLICATION_JSON)
-//                    .content(objectMapper.writeValueAsString(withdrawUserRequest))
-//                    .with(csrf()))
-//                .andExpect(status().isUnauthorized())
-//                .andDo(print());
-//    }
-//}
+package com.ideal402.urban;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ideal402.urban.api.dto.WithdrawUserRequest;
+import com.ideal402.urban.common.GlobalExceptionHandler;
+import com.ideal402.urban.common.ResourceNotFoundException;
+import com.ideal402.urban.config.SecurityConfig;
+import com.ideal402.urban.domain.entity.User;
+import com.ideal402.urban.domain.repository.UserRepository;
+import com.ideal402.urban.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.willThrow;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
+
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+@Import({GlobalExceptionHandler.class, SecurityConfig.class})
+public class UserApiTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private UserService userService;
+
+    @MockitoBean
+    private UserRepository userRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+
+    @Test
+    @DisplayName("setAlarm: 정상 동작")
+    @WithMockUser(username = "test@email.com")
+    public void setAlarmTest() throws Exception {
+
+        // given: 컨트롤러가 이메일과 지역ID를 서비스로 넘기는지 확인
+        willDoNothing().given(userService).addAlarm(anyString(), eq(1));
+
+        // when & then
+        mockMvc.perform(post("/users/me/alarms/{regionId}", 1)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf())) // POST 요청 시 CSRF 토큰 필요
+                .andExpect(status().isCreated())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("setAlarm: 존재하지 않는 지역 - 404")
+    @WithMockUser(username = "test@email.com")
+    public void setAlarmNotFoundTest() throws Exception {
+        // given: 예외 발생 시나리오
+        willThrow(new ResourceNotFoundException("존재하지 않는 지역입니다."))
+                .given(userService).addAlarm(anyString(), eq(1000));
+
+        // when & then
+        mockMvc.perform(post("/users/me/alarms/{regionId}", 1000)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf()))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("deleteAlarm: 정상 요청")
+    @WithMockUser(username = "test@email.com")
+    public void deleteAlarmTest() throws Exception {
+
+        // given
+        willDoNothing().given(userService).deleteAlarm(anyString(), eq(1));
+
+        // when & then
+        mockMvc.perform(delete("/users/me/alarms/{regionId}", 1)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf()))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("withdrawUser: 정상 요청")
+    @WithMockUser(username = "test@email.com")
+    public void withdrawUserTest() throws Exception {
+
+        // given
+        WithdrawUserRequest request = new WithdrawUserRequest().password("password123");
+
+        willDoNothing().given(userService)
+                .withdrawUser(anyString(), anyString(), any(HttpServletRequest.class));
+
+        // when & then
+        mockMvc.perform(delete("/users/me")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                        .with(csrf()))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("withdrawUser: 비밀번호 불일치 - 403")
+    @WithMockUser(username = "test@email.com")
+    public void withdrawUserForbiddenTest() throws Exception {
+
+        // given
+        WithdrawUserRequest request = new WithdrawUserRequest().password("wrongPassword");
+
+        willThrow(new AccessDeniedException("비밀번호가 일치하지 않습니다."))
+                .given(userService)
+                .withdrawUser(anyString(), anyString(), any(HttpServletRequest.class));
+
+        // when & then
+        mockMvc.perform(delete("/users/me")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                        .with(csrf()))
+                .andExpect(status().isForbidden()) // 403
+                .andDo(print());
+    }
+    @Test
+    @DisplayName("withdrawUser: 인증토큰 만료/없음 - 401")
+    public void withdrawUserUnauthorizedTest() throws Exception {
+
+        // given
+        WithdrawUserRequest request = new WithdrawUserRequest().password("password");
+
+        // when & then
+        mockMvc.perform(delete("/users/me")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                        .with(csrf()))
+                .andExpect(status().isUnauthorized()) // 401
+                .andDo(print());
+    }
+}
