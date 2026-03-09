d62261a4aff3c04c254efa62d2874bf1333e008b
ideal402 <ideal402@naver.com>
2026-03-10 02:26:23 +0900

    신규 워커 스레드 할당 시 인증정보 할당

--- project-api/src/main/java/com/ideal402/urban/config/ThreadPoolConfig.java
+++ project-api/src/main/java/com/ideal402/urban/config/ThreadPoolConfig.java
@@ -3,6 +3,7 @@ package com.ideal402.urban.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.concurrent.DelegatingSecurityContextExecutor;
 
 import java.util.concurrent.Executor;
 
@@ -20,6 +21,6 @@ public class ThreadPoolConfig {
         executor.setAwaitTerminationSeconds(30);
         executor.initialize();
 
-        return executor;
+        return new DelegatingSecurityContextExecutor(executor);
     }
 }
