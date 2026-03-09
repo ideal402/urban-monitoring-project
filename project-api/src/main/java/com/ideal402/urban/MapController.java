cb7b98892b5878315ca77d7c9bc1ad8abfef29b0
ideal402 <ideal402@naver.com>
2026-03-10 02:27:05 +0900

    map 컨트롤러 벌크해드 패턴 적용

--- project-api/src/main/java/com/ideal402/urban/MapController.java
+++ project-api/src/main/java/com/ideal402/urban/MapController.java
@@ -24,47 +24,35 @@ public class MapController{
     private final Executor asyncExecutor;
 
     @GetMapping("/current")
-    public CompletableFuture<ResponseEntity<List<CustomMapInfo>>> getMapData(
+    public ResponseEntity<List<CustomMapInfo>> getMapData(
             @RequestParam(value = "regionId", required = false) Integer regionId,
-            @RequestParam(value = "minLat", required = false) Double minLat, // 최소 위도 (남단)
-            @RequestParam(value = "maxLat", required = false) Double maxLat, // 최대 위도 (북단)
-            @RequestParam(value = "minLon", required = false) Double minLon, // 최소 경도 (서단)
-            @RequestParam(value = "maxLon", required = false) Double maxLon  // 최대 경도 (동단)
+            @RequestParam(value = "minLat", required = false) Double minLat,
+            @RequestParam(value = "maxLat", required = false) Double maxLat,
+            @RequestParam(value = "minLon", required = false) Double minLon,
+            @RequestParam(value = "maxLon", required = false) Double maxLon
     ) {
-        return CompletableFuture.supplyAsync(() -> {
-            return mapService.getMapData(regionId, minLat, maxLat, minLon, maxLon);
-        }, asyncExecutor).thenApply(mapData -> {
-            return ResponseEntity.ok(mapData);
-        });
+        return ResponseEntity.ok(mapService.getMapData(regionId, minLat, maxLat, minLon, maxLon));
     }
 
     @GetMapping("/forecast")
-    public CompletableFuture<ResponseEntity<List<ForecastInfo>>> getMapForecast(
+    public ResponseEntity<List<ForecastInfo>> getMapForecast(
             @RequestParam(value = "regionId", required = false) Integer regionId) {
-        return CompletableFuture.supplyAsync(() -> {
-            return mapService.getForecastData(regionId);
-        }, asyncExecutor).thenApply(forecastData -> {
-            return ResponseEntity.ok(forecastData);
-        });
+        return ResponseEntity.ok(mapService.getForecastData(regionId));
     }
 
     @GetMapping("/summary")
-    public CompletableFuture<ResponseEntity<List<MapInfo>>> getRegionSummary(
+    public ResponseEntity<List<MapInfo>> getRegionSummary(
             @RequestParam(value = "regionId", required = true) Integer regionId) {
-        return CompletableFuture.supplyAsync(() -> {
-            return mapService.getRegionSummary(regionId);
-        }, asyncExecutor).thenApply(summaryData -> {
-            return ResponseEntity.ok(summaryData);
-        });
+        return ResponseEntity.ok(mapService.getRegionSummary(regionId));
     }
 
     @PostMapping("/roads/traffic")
     public CompletableFuture<ResponseEntity<List<RoadResponse>>> getRoadTrafficInArea(@RequestBody List<String> h3Indices) {
         return CompletableFuture.supplyAsync(() -> {
-            return roadTrafficService.getRoadTrafficByH3Indices(h3Indices);
-        }, asyncExecutor)
-        .thenApply(trafficData -> {
-            return ResponseEntity.ok(trafficData);
-        });
+                    return roadTrafficService.getRoadTrafficByH3Indices(h3Indices);
+                }, asyncExecutor)
+                .thenApply(trafficData -> {
+                    return ResponseEntity.ok(trafficData);
+                });
     }
 }
